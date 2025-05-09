17b5a752b39ea7deb7f013d652d046b793d57bd0
Marian <115044973+mgorno@users.noreply.github.com>
2025-05-09 00:30:16 -0300

    reorganizo index

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+   window.addEventListener('DOMContentLoaded', () => {
+      const fadeElements = document.querySelectorAll('.fade-in-up, .fade-in-left');
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+          }
+        });
+      });
+      fadeElements.forEach(el => observer.observe(el));
+    });
+
+    const burger = document.getElementById('hamburgerBtn');
+    const menu = document.getElementById('menuYoga');
+    burger.addEventListener('click', () => {
+      burger.classList.toggle('active');
+      menu.classList.toggle('show');
+    });
